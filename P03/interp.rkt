9f001be16b9467b685186ec2c93497259536602e
Fer70 <72678703+Fer70@users.noreply.github.com>
2024-04-09 20:40:23 -0600

    Update interp.rkt

--- P03/interp.rkt
+++ P03/interp.rkt
@@ -65,8 +65,33 @@
 ;(subst 'x (num 1) (parse '{with {{a 1} {x x} {b x}} x}))
 ;(subst 'x (num 1) (with 'a (num 1) (with 'x (id 'x) (with 'b (id 'x) (id 'x)))))
 
+
+;Función auxiliar tomada del laboratorio para aplicar a cada with de la lista las propiedades del with
+(define (with*->with bs cuerpo)
+  (match bs
+    ['() cuerpo]
+    [(cons x xs) (with (binding-id x) (binding-value x) (with*->with xs cuerpo))]))
+
 ;; Parametros:
 ;;  - expr: WAE
 ;; Tipo del valor de retorno: number ó boolean ó string
 (define (interp expr)
-  (error 'interp "Sin implementar"))
+  (type-case WAE expr
+    [id (x) (error 'interp "Variable libre")] ;;Se regresa id pues no tiene valor
+    [num (x) x] ;;Se regresa un número 
+    [str (x) x] ;; Se regresa una cadena
+    [bool (x) x] ;; Se regresa un booleano
+    [noT (x) (let ([v (interp x)]) ;;Se checa que sea booleano y regresa el valor o error si es de otro tipo
+               (if (boolean? v)
+                   (not x)
+                   (error 'interp "El valor de not tiene que ser del tipo bool")))]
+    [with (id valor cuerpo) (let ([aid (interp id)]) ;; Se regresa la evaluación o error en caso de no usar un identificador correcto
+                              (if (id? aid)
+                                  (interp (subst id (interp valor) cuerpo))
+                                  (error "No es un identificador")))]
+                            
+    [op (f izq der) (let([val (interp izq)]) (let ([val1 (interp der)]) ;; Se realiza la operación indicada y se checa que sean números
+                      (if(and (num? val) (num val1))
+                         (f (interp izq) (interp der))
+                           (error "No es número"))))]
+    [with* (bindings cuerpo) (interp (with*->with bindings cuerpo))])) ;;Se regresa la asignación de cada uno de los withs

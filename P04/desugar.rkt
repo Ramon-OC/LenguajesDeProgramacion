b73f9bafc5b7ff7e51943ea11168bc4217879599
Fer70 <72678703+Fer70@users.noreply.github.com>
2024-04-19 20:40:45 -0600

    desugar intento.rkt

--- P04/desugar.rkt
+++ P04/desugar.rkt
@@ -5,4 +5,34 @@
 
 ;; desugar :: CFSBWAE -> CFSBAE
 (define (desugar expr)
-  (error 'desugar "Sin implementar"))
+  (type-case CFSBWAE expr
+    [idS (i) (id i)]
+    [numS (n) (num n)]
+    [boolS (b) (bool b)]
+    [strS (s) (str s)]
+    [opS (oper arg) (op (map desugar arg))]
+    [withS (bind body) (desugar bind body)]
+    [with*S (bind body) (desugar bind body)]
+    [funS (ids body) ((map desugar ids) (desugar body))]
+    [appS (f args) ((desugar f) (map desugar args))]
+    [iFS (c t e) ((desugar c) (desugar t) (desugar e))]
+    [conDS (con els) (desugar-conds con els)]))
+
+(define (desugar-conds conds else)
+  (if (empty? conds)
+      empty
+      (iF (desugar (car conds)) (desugar-conds (cdr conds) else) (desugar else))))
+
+(define (desugar-withs bind body)
+  (app (fun(map desugar (id-take bind)) (desugar body)) (map desugar (val-take bind))))
+
+(define (id-take lis)
+  (if (empty? lis)
+      empty
+      (cons (car (car lis))(id-take (cdr lis)))))
+
+(define (val-take lis)
+  (if (empty? lis)
+      empty
+      (cons (cdr (car lis)) (val-take (cdr lis)))))
+
